b62a3bb202a382eba2bb6795d6751ec71ec225c4
Ramon Carvalho <rcarvalho.pb@gmail.com>
2026-01-01 21:49:43 -0300

    feat: create application worker

---
+++ internal/application/worker/payment_worker.go
@@ -0,0 +1,11 @@
+package worker
+
+import "github.com/rcarvalho-pb/payment_system-go/internal/domain/event"
+
+type PaymentWorker struct {
+	Handler PaymentHandler
+}
+
+type PaymentHandler interface {
+	Handle(event.Event) error
+}
